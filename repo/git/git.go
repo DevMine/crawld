43742d09bc82f40c7b75bac9d91f20b87fb0a125
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2015-01-07 15:50:09 +0100

    repo/git: check repo existence before clone/update operations
    When a repository is deleted on the remote (more generally, when the
    remote resource is not available), GitHub requires an authentication and
    git(1) prompts the user, which causes the fetcher to pause.

--- repo/git/git.go
+++ repo/git/git.go
@@ -7,8 +7,12 @@
 package git
 
 import (
+	"net/http"
+	"net/url"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 
 	"github.com/golang/glog"
 )
@@ -22,7 +26,6 @@ type GitRepo struct {
 
 // New creates a new GitRepo.
 func New(absPath string, url string) (*GitRepo, error) {
-
 	path, err := exec.LookPath("git")
 	if err != nil {
 		glog.Error(err)
@@ -44,6 +47,12 @@ func (gr GitRepo) URL() string {
 
 // Clone implements the Clone() method of the Repo interface.
 func (gr GitRepo) Clone() error {
+	if ok, err := gr.isAvailable(); err != nil {
+		return err
+	} else if !ok {
+		glog.Info(gr.url, " not available")
+		return nil
+	}
 
 	out, err := exec.Command(gr.gitBin, "clone", "--quiet", gr.url, gr.absPath).CombinedOutput()
 	glog.Info(string(out))
@@ -57,6 +66,12 @@ func (gr GitRepo) Clone() error {
 
 // Update implements the Update() method of the Repo interface.
 func (gr GitRepo) Update() error {
+	if ok, err := gr.isAvailable(); err != nil {
+		return err
+	} else if !ok {
+		glog.Info(gr.url, " not available")
+		return nil
+	}
 
 	err := os.Chdir(gr.absPath)
 	if err != nil {
@@ -72,4 +87,49 @@ func (gr GitRepo) Update() error {
 	}
 
 	return nil
+}
+
+// isAvailable queries the git repository in order to determine whether a the
+// repository exists and is publicly available.
+func (gr GitRepo) isAvailable() (bool, error) {
+	u, err := url.Parse(gr.url)
+	if err != nil {
+		return false, err
+	}
+
+	switch u.Scheme {
+	case "http", "https":
+		queryURL := cleanURL(gr.url)
+		if !hasGitExt(u.Path) {
+			queryURL += ".git"
+		}
+
+		resp, err := http.Get(gr.url + "/git-upload-pack")
+		if err != nil {
+			glog.Warning(err)
+			return false, nil
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			glog.Warningf("invalid HTTP status: expected %d, received %d",
+				http.StatusOK, resp.StatusCode)
+			return false, nil
+		}
+	default:
+		glog.Warningf("protocol %s not supported", u.Scheme)
+		return false, nil
+	}
+
+	return true, nil
+}
+
+// hasGitExt returns true if the path ends with a ".git" extension,
+// false otherwise.
+func hasGitExt(path string) bool {
+	return filepath.Ext(path) == ".git"
+}
+
+// cleanURL removes the trailing slashes of an URL, if any.
+func cleanURL(url string) string {
+	return strings.TrimSuffix(url, "/")
 }
